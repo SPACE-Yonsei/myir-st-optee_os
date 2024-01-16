73bd2ef06f8375aecdd2207a963799cc3d805d07
Sangwan Jeon <commits@swjeon.kr>
2024-01-16 22:20:34 +0900

    chore: update debug level

--- mk/config.mk
+++ mk/config.mk
@@ -67,7 +67,7 @@ CFG_TEE_CORE_DEBUG ?= y
 # 2: error + warning
 # 3: error + warning + debug
 # 4: error + warning + debug + flow
-CFG_TEE_CORE_LOG_LEVEL ?= 4
+CFG_TEE_CORE_LOG_LEVEL ?= 3
 
 # TA log level
 # If user-mode library libutils.a is built with CFG_TEE_TA_LOG_LEVEL=0,
